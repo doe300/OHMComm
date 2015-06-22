829f62164ab68b84637e3f826aa709e24b371a46
J0n8s <ZieglerJo53188@th-nuernberg.de>
2015-06-22 18:14:00 +0200

    Redesigned opus implementation
    Added header-guards and combined decoder and encoder into one processor

--- src/ProcessorOpus.cpp
+++ src/ProcessorOpus.cpp
@@ -1,6 +1,6 @@
 #include "ProcessorOpus.h"
 
-ProcessorOpus::ProcessorOpus(std::string name) : AudioProcessor(name)
+ProcessorOpus::ProcessorOpus(std::string name, int opusApplication, int ErrorCode) : AudioProcessor(name)
 {
 	//Opus supports this sampleRates: 8000, 12000, 16000, 24000, or 48000.
 	if (audioConfiguration.sampleRate == 8000 || audioConfiguration.sampleRate == 12000 || audioConfiguration.sampleRate == 16000 || audioConfiguration.sampleRate == 24000 || audioConfiguration.sampleRate == 48000)
@@ -20,4 +20,26 @@ ProcessorOpus::ProcessorOpus(std::string name) : AudioProcessor(name)
 	{
 		std::cout << "Device Channels not supported by Opus! Opus supports 1 or 2 Device Channels" << std::endl;
 	}
+	
+	OpusEncoderObject = opus_encoder_create(sampleRate, channels, opusApplication, &ErrorCode);
+	
+	OpusDecoderObject = opus_decoder_create(sampleRate, channels, &ErrorCode);
+	
+}
+uint32_t ProcessorOpus::encode(void *inputBuffer, unsigned int nBufferFrames, unsigned char *encodedPacket, opus_int32 maxPacketLength)
+{
+	uint32_t lengthEncodedPacket;
+	lengthEncodedPacket = opus_encode(OpusEncoderObject, (opus_int16 *)inputBuffer, nBufferFrames, encodedPacket, maxPacketLength);
+	return lengthEncodedPacket;
+}
+uint32_t ProcessorOpus::decode(unsigned char *encodedPacket, uint32_t lengthEncodedPacket, void *outputBuffer, unsigned int nBufferFrames)
+{
+	uint32_t numberOfDecodedSamples;
+	numberOfDecodedSamples = opus_decode(OpusDecoderObject, encodedPacket, lengthEncodedPacket, (opus_int16 *)outputBuffer, nBufferFrames, 0);
+	return numberOfDecodedSamples;
+}
+ProcessorOpus::~ProcessorOpus()
+{
+	opus_encoder_destroy(OpusEncoderObject);
+	opus_decoder_destroy(OpusDecoderObject);
 }
