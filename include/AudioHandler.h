ef9a2c575d0ca99aa6d6254b82302dd18927eb06
Kaayy-J <kamal_bhatti@hotmail.com>
2015-08-20 17:02:24 +0200

    Documentation
    Added comments (for doxygen)

--- include/AudioHandler.h
+++ include/AudioHandler.h
@@ -5,8 +5,9 @@
 #include "Statistics.h"
 #include "configuration.h"
 #include <vector>
-#include <memory> //std::unique_ptr
+#include <memory>
 #include <iostream>
+
 /*!
  * Base class for Audio framework.
  * 
@@ -16,36 +17,146 @@ class AudioHandler
 {
 public:
     virtual ~AudioHandler();
+
+	/*!
+	* Starts processing audio input from the microphone
+	*/
     virtual void startRecordingMode() = 0;
+
+	/*!
+	* Starts processing audio output to the speakers
+	*/
     virtual void startPlaybackMode() = 0;
+
+	/*!
+	* Starts processing audio input and output (default  for audio communication)
+	*/
     virtual void startDuplexMode() = 0;
 
+	/*!
+	* Configures the AudioHandler
+	*
+	* \param audioConfiguration The Object which contains the configuration
+	*/
     virtual void setConfiguration(const AudioConfiguration &audioConfiguration) = 0;
-    virtual void suspend() = 0; // suspends the stream
-    virtual void resume() = 0; // resume the stream
-    virtual void stop() = 0; // close the whole communication process
-    virtual void reset() = 0; // stop and reset audioConfiguration
-    virtual void playData(void *playbackData, unsigned int size) = 0;
-    virtual void setDefaultAudioConfig() = 0; // will load the default-config
+
+	/*!
+	* Suspends any processing
+	*/
+    virtual void suspend() = 0;
+
+	/*!
+	* Resumes a suspended state
+	*/
+    virtual void resume() = 0;
+
+	/*!
+	* Stops the communication process at all. The object cannot be resumed (Should be used on shutdown)
+	*/
+    virtual void stop() = 0;
+
+	/*!
+	* Stops and resets the audio configuration (Instance can be reused with a new audio configuration)
+	*/
+    virtual void reset() = 0; 
+
+	/*!
+	* Loads a default audio configuration
+	*/
+    virtual void setDefaultAudioConfig() = 0; 
+
     /*!
      * Initializes the audio library and prepares for execution.
      * 
      * This includes allocating memory, configuring the audio-processors or the audio-library, etc.
      */
     virtual auto prepare() -> bool = 0;
+
+	/*!
+	* Returns the actual buffer size, which can be different than in the audio configuration (
+	*/
     virtual auto getBufferSize() -> unsigned int = 0;
 
+	/*!
+	* Prints all included AudioProcessor in the processing order
+	*
+	* \param outputStream The outputStream for printing (default is std::cout)
+	*/
     void printAudioProcessorOrder(std::ostream& outputStream = std::cout) const;
+
+	/*!
+	* Adds a AudioProcessor to process chain
+	*
+	* \param audioProcessor The AudioProcessor to add
+	*
+	* \return The result of the action
+	*/
     auto addProcessor(AudioProcessor *audioProcessor) -> bool;
+
+	/*!
+	* Removes a AudioProcessor to process chain
+	*
+	* \param audioProcessor The AudioProcessor to remove
+	*
+	* \return The result of the action
+	*/
     auto removeAudioProcessor(AudioProcessor *audioProcessor) -> bool;
+
+	/*!
+	* Removes a AudioProcessor to process chain
+	*
+	* \param nameOfAudioProcessor The name of the AudioProcessor to remove
+	*
+	* \return The result of the action
+	*/
     auto removeAudioProcessor(std::string nameOfAudioProcessor) -> bool;
+
+	/*!
+	* Clears the process chain (removes all AudioProcessor's)
+	*
+	* \param nameOfAudioProcessor The name of the AudioProcessor to remove
+	*
+	* \return The result of the action
+	*/
     auto clearAudioProcessors() -> bool;
 
+	/*!
+	* Gives a information wheater an AudioProcessor is already added or not
+	*
+	* \param audioProcessor AudioProcessor object to check
+	*
+	* \return The result of the action
+	*/
     auto hasAudioProcessor(AudioProcessor *audioProcessor) const -> bool;
+
+	/*!
+	* Gives a information wheater the an AudioProcessor is already added or not
+	*
+	* \param nameOfAudioProcessor The name of the AudioProcessor to check
+	*
+	* \return The result of the action
+	*/
     auto hasAudioProcessor(std::string nameOfAudioProcessor) const -> bool;
+
+	/*!
+	* Returns the current AudioConfiguration
+	*
+	* \return Current AudioConfiguration
+	*/
     auto getAudioConfiguration() -> AudioConfiguration;
 
+	/*!
+	* Returns wheater the instance has an AudioConfiguration
+	*
+	* \return Status of the AudioConfiguration
+	*/
     auto isAudioConfigSet() const -> bool;
+
+	/*!
+	* Returns wheater instance is prepared (ready) for execution
+	*
+	* \return Prepared status of the instance
+	*/
     auto isPrepared() const -> bool;
 
 protected:
