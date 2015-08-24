1b3623bc45772d25c221e9202762b1b5442fe745
Kaayy-J <kamal_bhatti@hotmail.com>
2015-08-24 21:48:23 +0200

    Deactivated testmode

--- include/configuration.h
+++ include/configuration.h
@@ -8,8 +8,8 @@
 #ifndef CONFIGURATION_H
 #define	CONFIGURATION_H
 
-#define TESTMODE 1
-#define PACKET_LOSS_CHANCE_IN_PERCENT 50
+#define TESTMODE 0
+#define PACKET_LOSS_CHANCE_IN_PERCENT 0
 
 #if TESTMODE
 #include <stdio.h>
