befda426ee76f75969266f908cc7fc18b6f24314
doe300 <stadeldani@web.de>
2016-06-27 16:10:57 +0200

    Added some documentation for new features, see #70 Signed-off-by:doe300 <stadeldani@web.de>

--- include/sip/Authentication.h
+++ include/sip/Authentication.h
@@ -21,6 +21,8 @@ namespace ohmcomm
          * HTTP/SIP authentication
          * 
          * see: RFC 2617 https://tools.ietf.org/html/rfc2617
+         * 
+         * \since 0.9
          */
         class Authentication
         {
@@ -28,16 +30,48 @@ namespace ohmcomm
             Authentication();
             virtual ~Authentication();
             
+            /*!
+             * \param requestMethod The request-method required to authenticate
+             * 
+             * \param requestURI The URI of the request to authenticate
+             * 
+             * \param headerField The value of the WWW-Authenticate header-field
+             * 
+             * \return the applicable authentication method or an empty pointer
+             */
             static std::unique_ptr<Authentication> getAuthenticationMethod(const std::string& requestMethod, const std::string& requestURI, const std::string& headerField);
             
+            /*!
+             * \param userName The name to authenticate with
+             * 
+             * \param password The password to authenticate with
+             * 
+             * \return The value of the Authenticate header-field
+             */
             virtual std::string createAuthenticationHeader(const std::string& userName, const std::string& password) = 0;
             
+            /*!
+             * Returns whether the authentication was successful.
+             * 
+             * NOTE: An expired authentication will be treated as not authenticated
+             */
             bool isAuthenticated() const;
             
+            /*!
+             * Sets the authentication to successful and the time (in seconds) this authentication expires in
+             * 
+             * \param expirationTime The time (in seconds) from now, this authentication will expire on server-side
+             */
             void setAuthenticated(const std::chrono::system_clock::time_point expirationTime);
             
+            /*!
+             * \return whether this authentication has already expired
+             */
             bool isExpired() const;
             
+            /*!
+             * \return The time-point on which this authentication will expire
+             */
             std::chrono::system_clock::time_point getExpirationTime() const;
             
         private:
@@ -46,6 +80,13 @@ namespace ohmcomm
             std::chrono::system_clock::time_point expirationTime;
         };
         
+        /*!
+         * Basic HTTP/SIP authentication
+         * 
+         * Sends a Base64 encoded concatenation of user-name and password
+         * 
+         * NOTE: SIP actually does not allow this to be used (only digest authentication), but it was so easy to implement...
+         */
         class BasicAuthentication : public Authentication
         {
         public:
@@ -56,6 +97,14 @@ namespace ohmcomm
             std::string createAuthenticationHeader(const std::string& userName, const std::string& password) override;
         };
         
+        /*!
+         * Digest HTTP/SIP authentication
+         * 
+         * Uses a challenge send by the server and responds with a hashed version of the answer
+         * 
+         * NOTE: The default hash, MD5, is considered to be a WEAK hash and should therefore not be used anymore, but SIP 
+         * still requires it to be supported, so we do
+         */
         class DigestAuthentication : public Authentication
         {
         public:
